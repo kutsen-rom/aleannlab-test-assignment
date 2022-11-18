c3a336aff8e79443e797d522d1f007236c9a8b13
navsi100rom <navsi100rom@users.noreply.github.com>
2022-11-18 17:34:15 +0200

    Add color scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./src/**/*.tsx"],
   theme: {
-    extend: {},
+    extend: {
+      textColor: {
+        brand: "var(--color-brand)",
+        muted: "var(--color-muted)",
+        text: "var(--color-text)",
+        background: "var(--color-background)",
+        paragraph: "var(--color-paragraph)",
+      },
+      backgroundColor: {
+        brand: "var(--color-brand)",
+        background: "var(--color-background)",
+        "background-job": "var(--color-background-job)",
+      },
+    },
   },
   plugins: [],
 };
